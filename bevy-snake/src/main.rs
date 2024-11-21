4d4936f4627c485e4218ca2953cd735830b018d3
nfoj <nfoj_@hotmail.com>
2024-11-21 15:39:01 -0300

    bavy snake complet

--- bevy-snake/src/main.rs
+++ bevy-snake/src/main.rs
@@ -1,12 +1,12 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, time::common_conditions::on_timer, window::PrimaryWindow};
 use rand::prelude::random;
 use std::time::Duration;
 
-const ARENA_WIDTH: u32 = 20;
-const ARENA_HEIGHT: u32 = 20;
-const SNAKE_HEAD_COLOR: Color = Color::srgb(0.7, 0.7, 0.7);
-const SNAKE_SEGMENT_COLOR: Color = Color::srgba(0.3, 0.3, 0.3, 1.0);
-const FOOD_COLOR: Color = Color::srgb(1., 0., 1.);
+const ARENA_WIDTH: u32 = 120;
+const ARENA_HEIGHT: u32 = 120;
+const SNAKE_HEAD_COLOR: Color = Color::srgb(1., 1., 1.);
+const SNAKE_SEGMENT_COLOR: Color = Color::srgba(1., 1., 1., 0.2);
+const FOOD_COLOR: Color = Color::srgb(1., 1., 1.);
 
 #[derive(Component, Copy, Clone, PartialEq, Eq)]
 struct Position {
@@ -56,7 +56,7 @@ struct SnakeHead {
 #[derive(Component)]
 struct SnakeSegment;
 
-#[derive(Component)]
+#[derive(Resource, Default)]
 struct SnakeSegments(Vec<Entity>);
 
 #[derive(Component)]
@@ -73,16 +73,16 @@ struct GameOverEvent;
 
 struct Snake;
 
-// camera
+//
 impl Plugin for Snake {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, (setup_camera, spawn_snake).chain());
         app.add_systems(
             FixedUpdate,
             (
-                food_spawner.run_if(on_timer(Duration::from_secs(1))),
+                food_spawner.run_if(on_timer(Duration::from_secs(5))),
                 (
-                    snake_movement.run_if(on_timer(Duration::from_millis(150))),
+                    snake_movement.run_if(on_timer(Duration::from_millis(100))),
                     snake_eating,
                     snake_growth,
                     game_over,
@@ -106,7 +106,8 @@ impl Plugin for Snake {
         }));
 
         //
-        app.insert_resource(ClearColor(Color::srgb(0., 0., 0.)));
+        app.insert_resource(ClearColor(Color::srgb(0., 0., 0.))); // background color
+        app.insert_resource(LastTailPosition::default());
         app.insert_resource(SnakeSegments::default());
         app.add_event::<GrowthEvent>();
         app.add_event::<GameOverEvent>();
@@ -138,7 +139,7 @@ fn spawn_snake(mut commands: Commands, mut segments: ResMut<SnakeSegments>) {
             })
             .insert(SnakeSegment)
             .insert(Position { x: 3, y: 2 })
-            .insert(Size::square(0.8))
+            .insert(Size::square(0.6))
             .id(),
         spawn_segment(commands, Position { x: 3, y: 2 }),
     ]);
@@ -156,7 +157,7 @@ fn spawn_segment(mut commands: Commands, position: Position) -> Entity {
         })
         .insert(SnakeSegment)
         .insert(position)
-        .insert(Size::square(0.65))
+        .insert(Size::square(0.6))
         .id()
 }
 
@@ -175,7 +176,7 @@ fn food_spawner(mut commands: Commands) {
             x: (random::<f32>() * ARENA_WIDTH as f32) as i32,
             y: (random::<f32>() * ARENA_HEIGHT as f32) as i32,
         })
-        .insert(Size::square(0.8));
+        .insert(Size::square(0.6));
 }
 
 // scaling
@@ -187,7 +188,7 @@ fn size_scaling(
         for (sprite_size, mut transform) in q.iter_mut() {
             transform.scale = Vec3::new(
                 sprite_size.width / ARENA_WIDTH as f32 * window.width() as f32,
-                sprite_size.height / ARENA_HEIGHT as f32 * window.heigth() as f32,
+                sprite_size.height / ARENA_HEIGHT as f32 * window.height() as f32,
                 1.0,
             );
         }
@@ -239,6 +240,94 @@ fn snake_movement_input(
 }
 
 // snake moviment
+fn snake_movement(
+    segments: ResMut<SnakeSegments>,
+    mut heads: Query<(Entity, &SnakeHead)>,
+    mut last_tail_position: ResMut<LastTailPosition>,
+    mut positions: Query<&mut Position>,
+    mut game_over_write: EventWriter<GameOverEvent>,
+) {
+    if let Some((head_entity, head)) = heads.iter_mut().next() {
+        let segment_positions = segments
+            .0
+            .iter()
+            .map(|e| *positions.get_mut(*e).unwrap())
+            .collect::<Vec<Position>>();
+        *last_tail_position = LastTailPosition(Some(*segment_positions.last().unwrap()));
+        let mut head_pos = positions.get_mut(head_entity).unwrap();
+
+        match &head.direction {
+            Direction::Left => head_pos.x -= 1,
+            Direction::Right => head_pos.x += 1,
+            Direction::Up => head_pos.y += 1,
+            Direction::Down => head_pos.y -= 1,
+        }
+
+        if head_pos.x < 0
+            || head_pos.y < 0
+            || head_pos.x as u32 >= ARENA_WIDTH
+            || head_pos.x as u32 >= ARENA_HEIGHT
+        {
+            game_over_write.send(GameOverEvent);
+        }
+
+        if segment_positions.contains(&head_pos) {
+            game_over_write.send(GameOverEvent);
+        }
+
+        segment_positions
+            .iter()
+            .zip(segments.0.iter().skip(1))
+            .for_each(|(pos, segment)| *positions.get_mut(*segment).unwrap() = *pos);
+    }
+}
+
+//
+fn snake_growth(
+    commands: Commands,
+    last_tail_position: Res<LastTailPosition>,
+    mut segments: ResMut<SnakeSegments>,
+    mut growth_reader: EventReader<GrowthEvent>,
+) {
+    if growth_reader.read().next().is_some() {
+        segments
+            .0
+            .push(spawn_segment(commands, last_tail_position.0.unwrap()));
+    }
+}
+
+//
+fn snake_eating(
+    mut commands: Commands,
+    mut growth_writer: EventWriter<GrowthEvent>,
+    food_positions: Query<(Entity, &Position), With<Food>>,
+    head_positions: Query<&Position, With<SnakeHead>>,
+) {
+    for head_pos in head_positions.iter() {
+        for (food_entity, food_pos) in food_positions.iter() {
+            if food_pos == head_pos {
+                commands.entity(food_entity).despawn();
+                growth_writer.send(GrowthEvent);
+            }
+        }
+    }
+}
+
+// GameOver
+fn game_over(
+    mut commands: Commands,
+    mut reader: EventReader<GameOverEvent>,
+    segments_res: ResMut<SnakeSegments>,
+    food: Query<Entity, With<Food>>,
+    segments: Query<Entity, With<SnakeSegment>>,
+) {
+    if reader.read().next().is_some() {
+        for ent in food.iter().chain(segments.iter()) {
+            commands.entity(ent).despawn();
+        }
+        spawn_snake(commands, segments_res);
+    }
+}
 
 // main
 fn main() {
--- bevy-snake/step-by-step.md
+++ bevy-snake/step-by-step.md
@@ -20,6 +20,25 @@
      
 ```
 
+- Test
+
+```
+  // install watch
+  cargo install cargo-watch
+
+  // run
+  cargo watch -x run
+
+  // path > modification
+  cargo watch -w src -x run
+
+  // ignore files
+  cargo watch -w src -i target -x run
+
+  // https://crates.io/crates/cargo-watch
+  
+```
+
 - Dependencies
 
 ```
@@ -38,7 +57,8 @@
   bevy = "0.14.2"
   rand = "0.8.5"
 
-  // acess: https://crates.io/crates/rand
+  // https://bevyengine.org/ 
+  // https://crates.io/crates/rand
 
 ```
 
@@ -46,25 +66,24 @@ bevy = "0.14.2" : bevy game
 rand = "0.8.5" : random number generators
 
 
-
-- Use
-
-
-- Const
+- Const 
 
 ```
   
   // interger : i8 - i64 | u8 - u64
-  const AREA_WIDTH : u32 = 20; 
-  const AREA_HEIGHT : u32 = 20;
+  // The lower the number, the bigger the snake and the food
+  const AREA_WIDTH : u32 = 120; // test 
+  const AREA_HEIGHT : u32 = 120; // test
 
-  
-  // Color::srgba(red: f32, green: f32, blue: f32, alpha: f32); 
-  // Color (0-1) : 0. - 1. = (1., 0.2, 0.7)
+  // Color::srgba(red: f32, green: f32, blue: f32, alpha: f32);
+  // Color::srgb(red: f32, green: f32, blue: f32);
+  // Color (0-1) : 0 - 1 = (0.2, 0.7, 1.)
   const SNAKE_HEAD_COLOR : Color = Color::srgb(0.9, 0.6, 0.5);
-  
+  const SNAKE_SEGMENT_COLOR : Color = Color::srgb(0.3, 0.5, 0.);
   
 ```
+
+alpha = transparency
 
 
 - #[derive(...)]
@@ -76,3 +95,157 @@ Copy: The type can be copied simply and without additional cost.
 Clone: The type can be explicitly duplicated using the .clone() method.
 PartialEq: The type can be compared for partial equality.
 Eq: The type can be compared for total equality.
+
+- Example:
+
+```
+  // Debug > Derive
+  #[derive(Debug)]
+  struct Foo {
+    a:i32,
+  } 
+
+
+  // Debug not Derive
+  use std::fmt;
+
+  struct Foo {
+    a:i32,
+  }
+
+  impl fmt::Debug for Foo {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+      write!(f, "Foo: {}", self.a)
+    }
+  }
+  
+```
+
+- Position 
+
+```
+  #[derive(Component, Copy, Clone, PartialEq, Eq)]
+struct Position {
+    x: i32,
+    y: i32,
+  }
+
+  // Compoment: tag types as entity components in the ECS system, representing the position of this entity in space.
+  // Copy: allows position values to be copied rather than moved;
+  // Clone: allows you to create an explicit copy of a value;
+  // PartialEq: allows you to compare position values using the == (equality) operator; // Compare x and y
+  // Eq: ensures that equality is total(that is, if a == b, that b == a).
+  
+```
+
+- Size
+
+```
+  #[derive(Component)]
+  struct Size {
+      width: f32,
+      height: f32,
+  }
+  
+```
+
+- Direction
+
+```
+  #[derive(Clone, Copy, Debug, PartialEq)]
+  enum Direction {
+      Left,
+      Up,
+      Right,
+      Down,
+  }
+
+  // Debug: allows the direction type to be printed with the {:?} format, making debugging easier;
+  // PartialEq: check if two directions are equal, as if they point to the same direction (e. g., check if both are Left or Up).
+
+  impl Direction {
+    fn opposite(self) -> Self {
+        match self {
+            Self::Left => Self::Right,
+            Self::Right => Self::Left,
+            Self::Up => Self::Down,
+            Self::Down => Self::Up,
+        }
+    }
+}
+  
+```
+
+- SnakeHead
+
+```
+  #[derive(Component)]
+  struct SnakeHead {
+    direction: Direction,
+  }
+
+  // stores the direction of the snake's head in the game.
+  
+```
+
+- SnakeSegment
+
+```
+  #[derive(Component)]
+  struct SnakeSegment;
+
+  // store the direction of the snake's body in the game.
+  
+```
+
+- SnakeSegments
+
+```
+  #[derive(Resource, Default)]
+  struct SnakeSegments(Vec<Entity>);
+
+  // Resource: used to store data that is shared globally
+  // Default: Allows initializing a SnakeSegments with an empty vector (Vec<Entity>), useful when you want to start with an empty list of snake segments.
+  
+```
+
+- Food
+
+```
+  #[derive(Component)]
+  struct Food;
+
+  // represent pieces of food in the game.
+  
+```
+
+- GrowthEvent
+
+```
+  #[derive(Event)]
+  struct GrowthEvent;
+
+  // used to signal that the snake has grown.
+  
+```
+
+- LastTailPosition
+
+```
+  #[derive(Resource, Default)]
+  struct LastTailPosition(Option<Position>);
+
+  // Resource: stores the snake's tail's previous position. This is useful for knowing where the tail was before it grew;
+  // Default: initialized with a default value, which in this case is None.
+  
+```
+
+- GameOverEvent
+
+```
+  #[derive(Event)]
+  struct GameOverEvent;
+
+  // used to indicate that the game is over.
+  
+```
